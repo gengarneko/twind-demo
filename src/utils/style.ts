15a218e7a1c63a5bfa11e0184ac9ddc6607b2adc
XuYuCheng <xuyucheng@tezign.com>
2022-01-07 17:05:14 +0800

    feat: remove useless style module

--- src/utils/style.ts
+++
@@ -1,6 +0,0 @@
-import { setup } from "twind";
-import twindConfig from "../twind.config";
-
-export * from "twind/css";
-export { default as cx } from "clsx";
-export const initTheme = () => setup(twindConfig);
