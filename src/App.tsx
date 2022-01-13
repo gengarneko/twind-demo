e83b6b9c135c1fc8264d7bbacf12ccb6218e1a39
XuYuCheng <xuyucheng@tezign.com>
2022-01-13 15:00:19 +0800

    feat(qew): qweqwe

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React, { FC } from "react";
-import twindConfig from "./twind.config";
 import { setup, tw } from "twind/css";
+import twindConfig from "./twind.config";
+import { Comp } from "./Comp";
 
 setup(twindConfig);
 
-export const App: FC = () => {
-  return (
-    <div
-      className={tw`text-primary border rounded ${"customClassName"}`}
-    >
-      twind demo
-    </div>
-  );
-};
+export const App: FC = () => (
+  <div className={tw`w-full h-screen flex items-center justify-center`}>
+    <Comp />
+  </div>
+);
--- src/Comp.tsx
+++ src/Comp.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from "react";
 import { css, tw } from "twind/css";
+import { style } from "twind/style";
+
+const customClass = style(
+  div({
+    tw: `p-10`,
+  })
+);
 
 // * --------------------------- comp
 
-export const Comp: FC = () => {
-  return (
-    <div className={tw`flex ${customStyle}`}>
-      ... ... ...
-    </div>
-  );
-};
+export const Comp: FC = () => (
+  <div className={tw`border text-center text(center 3xl primary) w-[500px] ${"customClassName"} ${customStyle}`}>
+    twind demo
+  </div>
+);
 
 // * --------------------------- style
 
